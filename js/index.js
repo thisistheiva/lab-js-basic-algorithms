daba642bbf7fbe60e1e098a2efc6db84dcbe4a45
thisistheiva <theivasubu1725@gmail.com>
2023-10-29 09:31:30 -0700

    iteration 3 completed

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ hacker2 = "Bruce Lee"
 console.log("The navigator's name is " + hacker2)
 
 // Iteration 2: Conditionals
+
 if(hacker1.length > hacker2.length)
 {
     console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
@@ -17,4 +18,33 @@ else if(hacker1.length < hacker2.length)
 else{
     console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
 }
+
 // Iteration 3: Loops
+
+//Printing char with space in capital letter
+for(let i = 0; i <= hacker1.length; i++){
+    console.log(hacker1.charAt(i).toUpperCase())
+    console.log("")
+}
+
+//reverse string
+for(let j = hacker2.length ; j >= 0 ; j--){
+    console.log(hacker2.charAt(j).trim())
+}
+
+//lexicographic order
+let minlen = Math.min(hacker1.length, hacker2.length)
+for(let k = 0; k < minlen; k++){
+    if(hacker1.charAt(k) < hacker2.charAt(k)){
+        console.log("The driver's name goes first.")
+        break;
+    }
+    else if(hacker1.charAt(k) > hacker2.charAt(k)){
+        console.log("Yo, the navigator goes first definitely.")
+        break;
+    }
+    else{
+        console.log("What?! You both have the same name?")
+        break;
+    }
+}
