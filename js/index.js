2b3963d5be43dc6ca2c9417d46794ad652b851ed
thisistheiva <theivasubu1725@gmail.com>
2023-10-28 23:27:00 -0700

    iteration 1 completed

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 // Iteration 1: Names and Input
-//
+
+hacker1 = "Jackie Chan"
+console.log("The driver's name is" + hacker1)
+hacker2 = "Bruce Lee"
+console.log("The navigator's name is" + hacker2)
+
 // Iteration 2: Conditionals
 
 
